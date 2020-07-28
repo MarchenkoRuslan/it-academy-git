96b00216f1684a6157aff69c3c10a7d7d63e5ac1
MarchenkoRuslan <n3mo07@bk.ru>
2020-07-28 21:35:34 +0300

    fix fix

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 print("I’m planning to learn git conflicts resolution")
-<<<<<<< HEAD
 name = input('Please enter your name:')
 surname = input('Please enter your surname:')
 nickname = input('Please enter your nickname')
 print('Hi, {} {} a.k.a. {}!'.format(name, surname, nickname))
->>>>>>> RuslanMarchenko
+
